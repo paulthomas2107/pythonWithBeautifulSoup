d99f00bc0127050894eff5df04afa100a2bf2ed8
Paul Thomas <paulthomas2107@gmail.com>
2021-09-06 14:47:49 +0100

    More....

---
+++ web_scraping.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+
+with open("index.html", "r") as f:
+    doc = BeautifulSoup(f, "html.parser")
+
+print(doc.prettify())
+
+tag = doc.title
+print(tag)
+print(tag.string)
+
+tag.string = "Paul's File"
+print(tag)
+
+tag = doc.find("a")
+print (tag)
+
+tag = doc.find_all("p")
+print (tag)
+print (tag[0])
+
+tag = doc.find_all("b")
+print (tag)
+print (tag[0])
+
+
+url = "https://www.newegg.ca/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Description=3080&cm_re=3080-_-14-932-436-_-Product"
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+prices = doc.find_all(text="$")
+parent = prices[0].parent
+strong = parent.find("strong")
+print(strong)
+print(strong.string)
